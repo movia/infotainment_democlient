a8018faa1795a20b9eb08826b3d3c6d4f66f9900
Jens Just Iversen <jens@makeable.dk>
2019-01-21 13:54:05 +0100

    Fixed bug regarding marquee on displaymode 3

--- phpproxy/src/index.php
+++ phpproxy/src/index.php
@@ -29,7 +29,7 @@ if ($request->getMethod() === 'OPTIONS') {
   exit;
 }
 
-// getTestResponse(7);
+// getTestResponse(8);
 
 // Forward the request and get the response.
 $response = $proxy->forward($request)->to('https://wsilb.moviatrafik.dk' . urldecode($_GET['url']));
